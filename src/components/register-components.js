35443ae62c74bf133644ded8ae0ac128a2c93463
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 16:59:17 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -9,7 +9,6 @@ registerComponents({
     // Override any static or dynamic component,
     // or register your own dynamic component.
     MediaGallerySection: dynamic(() => import('./MediaGallerySection.tsx'))
-
 });
 
 /**
